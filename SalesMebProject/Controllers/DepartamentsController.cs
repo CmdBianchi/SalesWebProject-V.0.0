8dfde6a9f9ad0b8831a7a5fb2ab520b1009c9973
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-10 16:30:10 -0300

    Adicionar arquivos de projeto.

---
+++ SalesMebProject/Controllers/DepartamentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesMebProject.Models;
+namespace SalesMebProject.Controllers {
+    public class DepartamentsController : Controller {
+        public IActionResult Index() {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ SalesMebProject/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace SalesMebProject.Models {
+    public class Departament {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
