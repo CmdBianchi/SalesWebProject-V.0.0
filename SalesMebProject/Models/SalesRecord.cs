6cc5bbc1c9286e486da92ab50318a1fb35da92ae
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-11 10:13:42 -0300

    Commit 251.0 Add - > Enums/SaleStatus ViewModels/Departament ViewModels/SalesRecord ViewModels/Seller

---
+++ SalesMebProject/Models/SalesRecord.cs
@@ -0,0 +1,14 @@
+﻿using SalesMebProject.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace SalesMebProject.Models {
+    public class SalesRecord {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+
+    }
+}
