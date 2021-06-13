9afccb1ff4b607cb4d5ab0727beca1548f1aaff6
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-13 09:57:42 -0300

    Commint 254.1 Change - \SellerService

--- SalesMebProject/Services/SellerService.cs
+++ SalesMebProject/Services/SellerService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using SalesMebProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-
 namespace SalesMebProject.Services {
     public class SellerService {
+        private readonly SalesMebProjectContext _context;
+        public SellerService(SalesMebProjectContext context) {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() {
+            return _context.Seller.ToList();
+        }
     }
 }
