dda5702a354f311585e71fb3342956dd3d500e48
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-14 08:50:20 -0300

    Commit 258 Add - \SellersController \SellerService

--- SalesMebProject/Controllers/SellersController.cs
+++ SalesMebProject/Controllers/SellersController.cs
@@ -26,5 +26,15 @@ namespace SalesMebProject.Controllers {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Delete(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null) {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
