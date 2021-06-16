9cff896e17f75f4d12f54de079623acfcca52c08
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-16 09:59:02 -0300

    Commit 262 Add - \Seller \Startup

--- SalesMebProject/Models/Seller.cs
+++ SalesMebProject/Models/Seller.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 namespace SalesMebProject.Models {
     public class Seller {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
         public int DepartmentId { get; set; }
--- SalesMebProject/Startup.cs
+++ SalesMebProject/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using SalesMebProject.Models;
 using SalesMebProject.Data;
 using SalesMebProject.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using System.Collections.Generic;
 
 namespace SalesMebProject {
     public class Startup {
@@ -41,6 +44,13 @@ namespace SalesMebProject {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService) {
+            var enUS = new CultureInfo("en-US");
+            var localizationOption = new RequestLocalizationOptions {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOption);
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 seedingService.Seed();
