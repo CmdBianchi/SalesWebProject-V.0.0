e0db631fdc527b823488730869d585962ce4debf
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-16 11:05:46 -0300

    Commit 263

--- SalesMebProject/Models/Seller.cs
+++ SalesMebProject/Models/Seller.cs
@@ -6,17 +6,22 @@ using System.Threading.Tasks;
 namespace SalesMebProject.Models {
     public class Seller {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} Name required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0}Name size should be between 3 and 60")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} Name required")]
         public string Email { get; set; }
 
         [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} Name required")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Base Salary")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
