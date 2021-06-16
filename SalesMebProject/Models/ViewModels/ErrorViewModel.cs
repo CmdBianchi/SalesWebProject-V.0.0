aec25a840ceb24928e1743584103a5379403160a
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-16 09:07:47 -0300

    Commit 261.0 Add - \ErrorViewModel \Error

--- SalesMebProject/Models/ViewModels/ErrorViewModel.cs
+++ SalesMebProject/Models/ViewModels/ErrorViewModel.cs
@@ -4,6 +4,7 @@ using System;
 namespace SalesMebProject.Models {
     public class ErrorViewModel {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
