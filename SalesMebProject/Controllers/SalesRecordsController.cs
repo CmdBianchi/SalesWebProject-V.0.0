5f99c902aaed4f90ec8ca6a0eb12b915ab0a5b70
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-16 18:03:09 -0300

    Commit 266

---
+++ SalesMebProject/Controllers/SalesRecordsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace SalesMebProject.Controllers {
+    public class SalesRecordsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        public IActionResult SimplesSearch() {
+            return View();
+        }
+        public IActionResult GroupSearch() {
+            return View();
+        }
+    }
+}
